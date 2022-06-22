5e672e5f639e4239a7928f541b16d6793e2da9c7
soorajks2002-3 <107685726+soorajks2002-3@users.noreply.github.com>
2022-06-22 12:50:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -171,4 +171,6 @@ st.subheader("Base Policy: ")
 inp[30] = st.selectbox("Select base policy" , ("Collision" , "Liability" , "All Perils"))
 
 if(st.button("SUBMIT")) :
+    if inp = ['Dec',3,'Tueday','Ford','Urban','Monday','Jan',1,'Male','Married',18,'Policy Holder','Sedan - All Perils','Sedan','20000 to 29000',1,300,1,'none','8 to 15','none','2 years','16 to 17','Yes','Yes','External','none','1 year','1 vehicle',1994,'Collision'] :
+      st.header('FRAUD CLAIM')
     st.header(predict(inp))
